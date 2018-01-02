750fa2a0355805f1fe4023f24eedb842547febf5
mrosstech1962 <mrosstech@outlook.com>
2018-01-02 15:17:41 -0800

    New Site

---
+++ CICD/CICD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CICD.Startup))]
+namespace CICD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
